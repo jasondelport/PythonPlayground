46848e0db3dc42fe795cac7189322ad87d5e0cb0
Jason Delport <jason.delport@matrixbooking.com>
2022-06-14 10:35:15 +0100

    Learning Tuesdays

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
 # PythonPlayground    
+
 Python learning playground    
 
+check version (you can have both python 2 and python 3 installed on same machine)    
+* python --version
+* python3 --version
 
-brew install python.   
+install     
+* brew install python3    
 
-python3 src/main.py hello world     
+update    
+* brew update     
+* brew upgrade python3 
+* brew cleanup python3          
+  
+run script    
+* python3 src/main.py hello world     
 
+pip3    
+* pip3 freeze > requirements.txt     
+* pip3 install -r requirements.txt            
 
-pip3 freeze > requirements.txt     
-pip3 install -r requirements.txt            
-
-
-python3 -m http.server     
+run a server     
+* python3 -m http.server     
 
 
 https://docs.python-guide.org/intro/learning/        
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import os
+import platform
 import sys
+from contextlib import suppress
 
 import functions as func
 
@@ -8,10 +10,18 @@ print (f'version info -> {sys.version_info}')
 print (f'path -> {sys.path}')
 print (f'cwd -> {os.getcwd()}')
 
-for k, v in os.environ.items():
-    print(f'{k} -> {v}')
+print ('Python', platform.python_version())
 
-print (f'arguments -> {str(sys.argv)}')
+# this python 2 code breaks in python -> print "Hello, Python!"
+print ("Hello, Python!")
+
+name = "jason"
+print(f'Hello {name}!')
+
+#for k, v in os.environ.items():
+#    print(f'{k} -> {v}')
+
+#print (f'arguments -> {str(sys.argv)}')
 
 x = 3              # a whole number                   
 f = 3.1415926      # a floating point number              
@@ -31,3 +41,56 @@ squares = [1, 4, 9, 16, 25]
 print(squares)
 
 func.greetings()
+
+for i in range(5):
+    print(i)
+    i += 2
+
+some_letters = ['a', 'b', 'c', 'd', 'e']
+for letter in some_letters:
+    # do something
+    pass
+
+for i, letter in enumerate(some_letters, start=0):
+    print(f'item at index {i} is {letter}')
+
+#OLD WAY
+f = open('test.txt')  # note default is mode='r'
+print(f.read())
+f.close()
+
+#NEW WAY
+with open('test.txt') as f: # no close() needed
+   print(f.read())
+
+# handling errors
+try:
+    raise RuntimeWarning('Something could go wrong')
+except RuntimeWarning as e:  # as e is optional
+    print(e)
+
+empty_dict = {}  # or dict()
+my_dict = {'key': 'value'}
+# How to get value from dict
+try:
+    my_dict['a']  # raises KeyError if 'a' not in dictionary
+except:
+    print('error')
+
+print(my_dict.get('a', 'bob'))
+
+if 'key' in my_dict:
+    val = my_dict['key']
+val = my_dict.get('key', None)
+if val is not None: pass
+with suppress(KeyError):
+    val = my_dict['key']
+print(val)
+
+# multi lline comment
+"""
+In Python, the pass keyword is an entire statement in itself. 
+This statement doesn’t do anything: it’s discarded during the 
+byte-compile phase. But for a statement that does nothing, 
+the Python pass statement is surprisingly useful.
+"""
